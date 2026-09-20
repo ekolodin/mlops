85ad053460cbda8b4c5098055a6b38b8ec212a7e
efimilyan <efimmadroskii@mail.ru>
2026-09-20 14:31:01 +0300

    hw01: validate float64 dtype of inputs

--- homework/01-bindings/starter/cpp/bindings.cpp
+++ homework/01-bindings/starter/cpp/bindings.cpp
@@ -2,13 +2,20 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <stdexcept>
+#include <string>
 
 namespace py = pybind11;
 
+static void require_float64(const py::array& value, const char* name) {
+    if (!value.dtype().equal(py::dtype::of<double>())) {
+        throw py::type_error(std::string(name) + " must be float64");
+    }
+}
+
 py::array mac(const py::array& a, const py::array& b, const py::array& c) {
-    // TODO 2: validate dtype, ndim, matching shapes, contiguous/aligned buffers.
-    // Allocate an independent output, call mac_kernel, and return the array.
-    // The Python contract and exception types are specified in ../README.md.
+    require_float64(a, "a");
+    require_float64(b, "b");
+    require_float64(c, "c");
     throw std::logic_error("TODO 2: implement array binding");
 }
 
